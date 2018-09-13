ea597159bf7be2c2fed777dd463d5695f96ffc95
ej68 <ej68@duke.edu>
2018-09-13 12:52:20 -0400

    added apNameToLocation.csv (removed annoying row numbers) data_filter.R reads in data added 20_days.csv to gitignore

--- scripts/data_filter.R
+++ scripts/data_filter.R
@@ -1,12 +1,25 @@
 ## Reads in Data files (e.g. from box) and selects events with certain criteria
 ## Exports to new csv
 
-if(!require(dplyr)){
-  install.packages("dplyr")
+## Change stuff here
+fileNamesToReadIn <- c("20_days")
+locations <- "Perkins"
+##
+
+# Install Packages
+if(!require(tidyverse)){
+  install.packages("tidyverse")
   library(dplyr)
 }
 
 if(!require(readr)){
   install.packages("readr")
   library(readr)
+}
+
+apNameToLocation <- read_csv("./data/apNameToLocation.csv")
+
+for(fileName in fileNamesToReadIn){
+  # Read in data
+  eventData <- read_csv(paste0("./data/", fileName,".csv"))
 }
