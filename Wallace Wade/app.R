a85904bdad58fb88cf8d109aa5a4b4d30d7599e3
Eidan Jacob <ej68@duke.edu>
2018-09-23 16:02:58 -0400

    added background image

--- Wallace Wade/app.R
+++ Wallace Wade/app.R
@@ -23,19 +23,23 @@ if(!require(jpeg)){
   install.packages("jpeg")
   library(jpeg)
 }
+if(!require(grid)){
+  install.packages("grid")
+  library(grid)
+}
 
 # Global Variables
 building_folder <- "building_folder" # Name of building data folder
 height <- 600 # in px
 txtscl <- 1 # Text scaling factor (reduce if text too large)
-apsdf <- read_csv(
-  paste0("../data/",
-         building_folder,
-         "/apData.csv"))
+# apsdf <- read_csv(paste0("../data/",
+#                          building_folder,
+#                          "/apData.csv"))
 reportFile = "reportFile.txt"
 con <- file(reportFile, "w")
 cat("Access Points in Building Data that are missing from Event Data\n", file = con)
 close(con)
+backImage <- readJPEG("../data/wallaceWade/backImage.jpg")
 
 ui <- fluidPage(
   plotOutput("myPlot", height = height)
@@ -43,23 +47,23 @@ ui <- fluidPage(
 
 server <- function(input, output){
   
-observe({
-    # Count the number of events per access point. This is a crude measure of network load but it's good enough for students.
-    eventsdf <- read_csv(paste0("../data",building_folder,"/eventData.csv"))
-    chartData <- summarise(group_by(eventsdf, ap), n())
-    
-    # Are there access points with no events? If so, make a note in reportFile.
-    missing <- apsdf$ap[which( !(apsdf$ap %in% chartData$ap))]
-    if(length(missing) != 0){
-      missdf <- data.frame(missing, 0)
-      for(missingAP in missing){
-        con <- file(reportFile, "a")
-        cat(missingAP, as.character(Sys.time()), "\n", file = con, append = TRUE)
-        close(con)
-      }
-      names(missdf) <- names(chartData)
-      chartData <- rbind(chartData, missdf)
-    }
+  observe({
+    # # Count the number of events per access point. This is a crude measure of network load but it's good enough for students.
+    # eventsdf <- read_csv(paste0("../data",building_folder,"/eventData.csv"))
+    # chartData <- summarise(group_by(eventsdf, ap), n())
+    # 
+    # # Are there access points with no events? If so, make a note in reportFile.
+    # missing <- apsdf$ap[which( !(apsdf$ap %in% chartData$ap))]
+    # if(length(missing) != 0){
+    #   missdf <- data.frame(missing, 0)
+    #   for(missingAP in missing){
+    #     con <- file(reportFile, "a")
+    #     cat(missingAP, as.character(Sys.time()), "\n", file = con, append = TRUE)
+    #     close(con)
+    #   }
+    #   names(missdf) <- names(chartData)
+    #   chartData <- rbind(chartData, missdf)
+    # }
     
     # Code to generate plot
     output$myPlot <- renderPlot({
@@ -74,12 +78,11 @@ observe({
               axis.title.y = element_text(face = "bold", size = 20 * txtscl, family = "serif"), 
               axis.text.y = element_blank(), axis.ticks.y = element_blank(),
               plot.title = element_text(size = 30 * txtscl, hjust = 0.5),
-              plot.subtitle = element_text(size = 20 * txtscl, hjust = 0.5),
-              panel.background = element_rect(fill = "white")) +
-        # Add labels on floors and axes.
-        geom_text(aes(x = X, y = Y, label = text), data = labs, size = 8 * txtscl) +
-        scale_x_continuous(orientation["down"], sec.axis = sec_axis(~ . * 1, name = orientation["up"])) +
-        scale_y_continuous(orientation["left"], sec.axis = sec_axis(~ . * 1, name = orientation["right"]))
+              plot.subtitle = element_text(size = 20 * txtscl, hjust = 0.5)) +
+        annotation_custom(rasterGrob(backImage,
+                                     width = unit(1, "npc"),
+                                     height = unit(1, "npc")),
+                          -Inf, Inf, -Inf, Inf)
     })
   })
 }
